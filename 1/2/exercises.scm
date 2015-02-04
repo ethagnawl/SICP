14fef924f111cf1f8ed7135885a2ebe475c46873
Peter Doherty <pdoherty926@gmail.com>
2015-02-04 16:01:10 -0500

    completed exercise 1.10

--- 1/2/exercises.scm
+++ 1/2/exercises.scm
@@ -46,3 +46,52 @@ Exercise 1.9
 ; (my-other-plus (dec 1) (inc 8))
 ; (my-other-plus 0 9)
 ; 9
+
+
+#|
+Exercise 1.10
+|#
+(define (A x y)
+  (cond ((= y 0) 0)
+	((= x 0) (* 2 y))
+	((= y 1) 2)
+	(else (A (- x 1) (A x (- y 1))))))
+
+
+(A 1 10) ; 1024
+(A 2 4) ; 65536
+(A 3 3) ; 65536
+
+
+(define (f n) (A 0 n))
+
+(f 0) ; 0
+(f 1) ; 2
+(f 2) ; 4
+(f 3) ; 6
+
+
+(define (g n) (A 1 n))
+
+(g 0) ; 0
+(g 1) ; 2
+(g 2) ; 4
+(g 3) ; 8
+
+
+(define (h n) (A 2 n))
+
+(h 0) ; 0
+(h 1) ; 2
+(h 2) ; 4
+(h 3) ; 16
+(h 4) ; 65536
+
+
+(define (k n) (* 5 n n))
+
+(k 0) ; 0
+(k 1) ; 5
+(k 2) ; 20
+(k 3) ; 45
+(k 4) ; 80
