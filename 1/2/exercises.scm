799ca3bdf0390b9431eec01f8081be3112833b2b
Peter Doherty <pdoherty926@gmail.com>
2015-02-10 17:31:22 -0500

    completing 1.14

--- 1/2/exercises.scm
+++ 1/2/exercises.scm
@@ -166,3 +166,28 @@ Exercise 1.13
 ; not happening
 
 #|
+Exercise 1.14
+|#
+
+; space - linear - tree grows linearly with n
+; steps - O(Nm)
+
+(define (count-change amount)
+  (cc amount 5))
+(define (cc amount kinds-of-coins)
+  (cond ((= amount 0) 1)
+        ((or (< amount 0) (= kinds-of-coins 0)) 0)
+        (else (+ (cc amount
+                     (- kinds-of-coins 1))
+                 (cc (- amount
+                        (first-denomination kinds-of-coins))
+                     kinds-of-coins)))))
+(define (first-denomination kinds-of-coins)
+  (cond ((= kinds-of-coins 1) 1)
+        ((= kinds-of-coins 2) 5)
+        ((= kinds-of-coins 3) 10)
+        ((= kinds-of-coins 4) 25)
+        ((= kinds-of-coins 5) 50)))
+
+(count-change 11)
+
