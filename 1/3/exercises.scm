bd5d6b5b10667731e5457f1ac2fdd4ec2252b6b8
Peter Doherty <pdoherty926@gmail.com>
2015-02-14 13:31:02 -0500

    completing exercise 1.31

--- 1/3/exercises.scm
+++ 1/3/exercises.scm
@@ -72,3 +72,49 @@ Exercise 1.30
 (new-integral cube 0 1 0.001)
 
 
+#|
+Exercise 1.31
+|#
+
+; http://www.billthelizard.com/2010/05/sicp-exercise-131-product-of-series.html
+
+(define (square x) (* x x))
+
+(define (wallis-pi func n)
+  (define (term x)
+    (/ (* 4.0 (square x))
+       (- (* 4.0 (square x)) 1)))
+  (* 2.0 (func term 1 inc n)))
+
+(define (linear-recursive-product term a next b)
+  (if (> a b)
+    1
+    (*
+      (term a)
+      (linear-recursive-product term (next a) next b))))
+
+; (* 1 (* 2 (* 3 (* 4 5))))
+
+(define (inc x) (+ 1 x))
+
+(define (linear-recursive-factorial x) (linear-recursive-product identity 1 inc x))
+
+(linear-recursive-factorial 5) ; 120
+
+(wallis-pi linear-recursive-product 10) ; 3.0677038066434994
+
+(define (linear-iterative-product term a next b)
+  (letrec ([iter (lambda (a result)
+                   (if (> a b)
+                     result
+                     (iter (next a) (* (term a) result))))])
+    (iter a 1)))
+
+(define (linear-iterative-factorial x)
+  (linear-iterative-product identity 1 inc x))
+
+(linear-iterative-factorial 5) ; 120
+
+(wallis-pi linear-iterative-product 10) ; 3.0677038066434985
+                                        ; which differs sligtly from the
+                                        ; linear-recursive-product version
