ceccd91ba14ff5a46ff70b31f5b2995ea26b4535
Peter Doherty <pdoherty926@gmail.com>
2015-02-14 22:19:26 -0500

    completing exercise 1.33

--- 1/3/exercises.scm
+++ 1/3/exercises.scm
@@ -151,3 +151,52 @@ Exercise 1.32
 
 ; product
 ((lambda (a b) (linear-iterative-accumulate * 1 identity a inc b)) 1 5) ; 120
+
+
+#|
+Exercise 1.33
+|#
+
+; http://www.billthelizard.com/2010/05/sicp-exercise-133-filtered-accumulator.html
+
+(define (square x) (* x x))
+
+(define (gcd a b)
+  (if (= b 0)
+    a
+    (gcd b (remainder a b))))
+
+(define (fast-prime? n)
+  (define (smallest-divisor n)
+    (define (find-divisor n test-divisor)
+      (define (next x)
+        (if (= x 2) 3 (+ x 2)))
+      (define (divides? a b)
+        (= (remainder b a) 0))
+      (cond ((> (square test-divisor) n) n)
+            ((divides? test-divisor n) test-divisor)
+            (else (find-divisor n (next test-divisor)))))
+    (find-divisor n 2))
+  (= n (smallest-divisor n)))
+
+(define (filtered-accum filter combiner null-value term a next b)
+  (if (> a b)
+    null-value
+    (if (filter a)
+      (combiner (term a)
+                (filtered-accum filter combiner null-value term (next a) next b))
+      (filtered-accum filter combiner null-value term (next a) next b))))
+
+; sum even numbers
+((lambda (a b) (filtered-accum even? + 0 identity a inc b)) 0 10) ; 30
+
+; find the sum of the squares of the prime numbers in a given range
+((lambda (a b) (filtered-accum fast-prime? + 0 square a inc b)) 2 3) ; 13
+
+; find the product of all the positive integers less than n that are relatively prime to n
+(define (product-of-coprimes n)
+  (define (coprime? i) (= 1 (gcd i n)))
+  (filtered-accum coprime? * 1 identity 1 inc (- n 1)))
+
+(product-of-coprimes 10) ; 189
+(product-of-coprimes 11) ; 362880
