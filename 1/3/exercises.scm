be7e63a1941c1002a953f703e2d2ff8d051d360b
Peter Doherty <pdoherty926@gmail.com>
2015-02-17 11:26:35 -0500

    completing exercise 1.35

--- 1/3/exercises.scm
+++ 1/3/exercises.scm
@@ -218,3 +218,24 @@ Exercise 1.34
 ; (f 2)
 ; (2 2)
 
+
+#|
+Exercise 1.35
+|#
+
+; http://www.billthelizard.com/2010/07/sicp-exercise-135-fixed-points-and.html
+
+(define tolerance 0.00001)
+
+(define (fixed-point f first-guess)
+  (define (close-enough? v1 v2) (< (abs (- v1 v2)) tolerance))
+  (define (try guess)
+    (let ((next (f guess)))
+      (if (close-enough? guess next)
+        next
+        (try next))))
+  (try first-guess))
+
+(fixed-point (lambda (x) (+ 1 (/ 1 x))) 2.0)
+
+
