d2a083ac2653a2dc416b56b45c3f4d336c6ad3e0
Peter Doherty <pdoherty926@gmail.com>
2015-02-17 14:12:27 -0500

    completing exercise 1.40

--- 1/3/exercises.scm
+++ 1/3/exercises.scm
@@ -331,3 +331,45 @@ Exercise 1.39
 (tan (/ pi 6))        ; 0.5773502691896257
 
 (tan-cf (/ pi 6) 10)  ; 0.5773502691896257
+
+
+#|
+Exercise 1.40
+|#
+
+; http://www.billthelizard.com/2010/08/sicp-140-zeros-of-cubic-function.html
+
+(define tolerance 0.00001)
+
+(define (fixed-point f first-guess)
+  (define (close-enough? v1 v2)
+    (< (abs (- v1 v2)) tolerance))
+  (define (try guess)
+    (let ((next (f guess)))
+      (if (close-enough? guess next)
+        next
+        (try next))))
+  (try first-guess))
+
+(define dx 0.00001)
+
+(define (deriv g)
+  (lambda (x)
+    (/ (- (g (+ x dx)) (g x))
+       dx)))
+
+(define (newton-transform g)
+  (lambda (x)
+    (- x (/ (g x) ((deriv g) x)))))
+
+(define (newtons-method g guess)
+  (fixed-point (newton-transform g) guess))
+
+(define (cubic a b c)
+  (lambda (x)
+    (+ (* x x x)
+       (* a x x)
+       (* b x)
+       c)))
+
+(newtons-method (cubic 3 -2.4 6) 1) ; -3.9813366488305104
