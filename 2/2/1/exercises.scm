af7c328790979654f42095160eb743eabde8d81e
Peter Doherty <pdoherty926@gmail.com>
2015-02-24 14:56:43 -0500

    completing exercise 2.18

--- 2/2/1/exercises.scm
+++ 2/2/1/exercises.scm
@@ -12,3 +12,16 @@ Exercise 2.17
 (last-pair (list 23 72 149 32)) ; 32
 
 
+#|
+Exercise 2.18
+|#
+
+(define (my-reverse l)
+  (letrec ([iter (lambda (inner-l memo)
+                   (if (null? inner-l)
+                     memo
+                     (iter (cdr inner-l) (cons (car inner-l) memo))))])
+  (iter l (list))))
+
+(my-reverse (list 23 72 149 32)) ; (32 149 72 23)
+
