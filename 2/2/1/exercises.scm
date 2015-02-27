976356474651f09e65d99b0a2aa1d90914400152
Peter Doherty <pdoherty926@gmail.com>
2015-02-27 11:41:13 -0500

    completing exercise 2.26

--- 2/2/1/exercises.scm
+++ 2/2/1/exercises.scm
@@ -217,3 +217,16 @@ Exercise 2.25
     (cadadr '(1 (2 (3 (4 (5 (6 7))))))))) ; 7
 
 
+#|
+Exercise 2.26
+|#
+
+(define x (list 1 2 3))
+
+(define y (list 4 5 6))
+
+(append x y) ; (1 2 3 4 5 6)
+
+(cons x y) ; ((1 2 3) 4 5 6)
+
+(list x y); ((1 2 3) (4 5 6))
