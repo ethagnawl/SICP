de8eb758e011c557f2a5162fcb0811161b1fdc0d
Peter Doherty <pdoherty926@gmail.com>
2015-03-24 17:41:28 -0400

    completing exercise 2.44

--- 2/2/1/exercises.scm
+++ 2/2/1/exercises.scm
@@ -789,3 +789,40 @@ Exercise 2.43
  ; If it takes time T to execute the original version of queens for a given
  ; board size, we can expect the new version to take roughly Tboard-size time
  ; to execute.
+
+#|
+Exercise 2.44
+|#
+
+(define (flipped-pairs painter)
+  (let ((painter2 (beside painter (flip-vert painter))))
+    (below painter2 painter2)))
+
+(define (right-split painter n)
+  (if (= n 0)
+    painter
+    (let ((smaller (right-split painter (- n 1))))
+      (beside painter (below smaller smaller)))))
+
+(define (up-split painter n)
+  (if (= n 0)
+    painter
+    (let ((smaller (up-split painter (- n 1))))
+      (below painter (beside smaller smaller)))))
+
+(define (corner-split painter n)
+  (if (= n 0)
+    painter
+    (let ((up (up-split painter (- n 1)))
+          (right (right-split painter (- n 1))))
+      (let ((top-left (beside up up))
+            (bottom-right (below right right))
+            (corner (corner-split painter (- n 1))))
+        (beside (below painter top-left)
+                (below bottom-right corner))))))
+
+(define (square-limit painter n)
+  (let ((quarter (corner-split painter n)))
+    (let ((half (beside (flip-horiz quarter) quarter)))
+      (below (flip-vert half) half))))
+
